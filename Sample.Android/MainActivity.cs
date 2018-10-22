3af527c8a64468f0aa486f6d19ae6d7e9954bace
Daniel Hazelbaker <daniel@blueboxmoon.com>
2018-10-22 16:47:55 -0700

    Initial project import

---
+++ Sample.Android/MainActivity.cs
@@ -0,0 +1,19 @@
+﻿using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Support.V7.App;
+
+namespace Sample.Android
+{
+    [Activity( Label = "@string/app_name", Theme = "@style/AppTheme", MainLauncher = true )]
+    [IntentFilter( new[] { Intent.ActionMain }, Categories = new[] { Intent.CategoryLeanbackLauncher } )]
+    public class MainActivity : AppCompatActivity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            new Crex.Android.Application().Run( this );
+        }
+    }
+}
